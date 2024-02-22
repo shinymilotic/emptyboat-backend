c6f3d752e150f1b8ee70c32d9b6c7c5d9038c499
shinymilotic <trungtin.mai1412@gmail.com>
2024-02-22 10:27:24 +0700

    confirm email

--- src/main/java/overcloud/blog/controller/ApiConst.java
+++ src/main/java/overcloud/blog/controller/ApiConst.java
@@ -39,6 +39,8 @@ public class ApiConst {
 
     public static final String USER_LIST = "/userList";
 
+    public static final String CONFIRM_EMAIL = "/confirmEmail";
+
     public static final String TEST = "/test";
 
     public static final String TESTS = "/tests";
