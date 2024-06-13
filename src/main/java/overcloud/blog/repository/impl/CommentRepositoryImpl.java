d8f571a4d89131719b5ceb9532659c5bff34aba2
shinymilotic <trungtin.mai1412@gmail.com>
2024-06-13 22:51:59 +0700

    update

---
+++ src/main/java/overcloud/blog/repository/impl/CommentRepositoryImpl.java
@@ -0,0 +1,8 @@
+package overcloud.blog.repository.impl;
+
+import org.springframework.stereotype.Repository;
+import overcloud.blog.repository.ICommentRepository;
+
+@Repository
+public class CommentRepositoryImpl implements ICommentRepository {
+}
