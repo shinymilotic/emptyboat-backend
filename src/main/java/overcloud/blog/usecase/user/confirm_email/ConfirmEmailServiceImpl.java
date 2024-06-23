715be287ae4bdf2331bc7d87b70a00fb23dc65f5
shinymilotic <trungtin.mai1412@gmail.com>
2024-06-23 19:09:16 +0700

    update

--- src/main/java/overcloud/blog/usecase/user/confirm_email/ConfirmEmailServiceImpl.java
+++ src/main/java/overcloud/blog/usecase/user/confirm_email/ConfirmEmailServiceImpl.java
@@ -10,26 +10,31 @@ import overcloud.blog.repository.IUserRepository;
 import overcloud.blog.repository.jparepository.JpaRefreshTokenRepository;
 import overcloud.blog.usecase.common.auth.AuthResMsg;
 import overcloud.blog.usecase.common.exceptionhandling.InvalidDataException;
+import overcloud.blog.usecase.common.response.ResFactory;
+import overcloud.blog.usecase.common.response.RestResponse;
 import overcloud.blog.usecase.user.refresh_token.RefreshTokenRepository;
 
 import java.util.Optional;
 
 @Service
 public class ConfirmEmailServiceImpl implements ConfirmEmailService{
-
     private final JpaRefreshTokenRepository refreshTokenRepository;
-
+    private final ResFactory resFactory;
     private final IUserRepository userRepository;
-    public ConfirmEmailServiceImpl(JpaRefreshTokenRepository refreshTokenRepository, IUserRepository userRepository) {
+
+    public ConfirmEmailServiceImpl(JpaRefreshTokenRepository refreshTokenRepository,
+                                    IUserRepository userRepository,
+                                    ResFactory resFactory) {
         this.refreshTokenRepository = refreshTokenRepository;
         this.userRepository = userRepository;
+        this.resFactory = resFactory;
     }
 
     @Override
     @Transactional
-    public boolean confirmEmail(ConfirmEmailRequest confirmToken) {
+    public RestResponse<Void> confirmEmail(ConfirmEmailRequest confirmToken) {
         if (confirmToken == null || !StringUtils.hasText(confirmToken.getConfirmToken())) {
-            throw new InvalidDataException(AuthResMsg.AUTHORIZE_FAILED);
+            throw new InvalidDataException(resFactory.fail(AuthResMsg.AUTHORIZE_FAILED));
         }
 
         Optional<UserEntity> user = refreshTokenRepository.findUserByToken(confirmToken.getConfirmToken());
@@ -37,8 +42,9 @@ public class ConfirmEmailServiceImpl implements ConfirmEmailService{
         if (user.isPresent()) {
             this.userRepository.enableUser(confirmToken.getConfirmToken());
         } else {
-            throw new InvalidDataException(AuthResMsg.AUTHORIZE_FAILED);
+            throw new InvalidDataException(resFactory.fail(AuthResMsg.AUTHORIZE_FAILED));
         }
-        return true;
+        
+        return resFactory.success(AuthResMsg.AUTHORIZE_SUCCESS, null);
     }
 }
