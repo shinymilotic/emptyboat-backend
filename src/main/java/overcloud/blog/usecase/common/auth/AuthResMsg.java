1b7cbab19159cdce5a88b032beaa47530b458b44
shinymilotic <trungtin.mai1412@gmail.com>
2024-06-24 16:14:34 +0700

    update logout

--- src/main/java/overcloud/blog/usecase/common/auth/AuthResMsg.java
+++ src/main/java/overcloud/blog/usecase/common/auth/AuthResMsg.java
@@ -2,7 +2,4 @@ package overcloud.blog.usecase.common.auth;
 
 
 public class AuthResMsg {
-    public static final String AUTHORIZE_SUCCESS = "authorize.success";
-    public static final String AUTHORIZE_FAILED = "authorize.failed";
-    public static final String TOKEN_TIMEOUT = "authorize.token.timeout";
 }
--- src/main/java/overcloud/blog/usecase/user/logout/LogoutService.java
+++ src/main/java/overcloud/blog/usecase/user/logout/LogoutService.java
@@ -12,6 +12,9 @@ import org.springframework.transaction.annotation.Transactional;
 import overcloud.blog.core.cache.RedisUtils;
 import overcloud.blog.repository.jparepository.JpaRefreshTokenRepository;
 import overcloud.blog.usecase.common.auth.bean.SecurityUser;
+import overcloud.blog.usecase.common.response.ResFactory;
+import overcloud.blog.usecase.common.response.RestResponse;
+import overcloud.blog.usecase.user.common.UserResMsg;
 
 import java.util.Arrays;
 import java.util.Optional;
@@ -20,16 +23,15 @@ import java.util.Optional;
 public class LogoutService {
 
     private final JpaRefreshTokenRepository refreshTokenRepository;
+    private final ResFactory resFactory;
 
-    private final RedisUtils redisUtils;
-
-    public LogoutService(JpaRefreshTokenRepository refreshTokenRepository, RedisUtils redisUtils) {
+    public LogoutService(JpaRefreshTokenRepository refreshTokenRepository, ResFactory resFactory) {
         this.refreshTokenRepository = refreshTokenRepository;
-        this.redisUtils = redisUtils;
+        this.resFactory = resFactory;
     }
 
     @Transactional
-    public boolean logout(HttpServletRequest request, HttpServletResponse response) {
+    public RestResponse<Void> logout(HttpServletRequest request, HttpServletResponse response) {
         readServletCookie(request, "refreshToken")
                 .ifPresent(refreshTokenRepository::deleteByRefreshToken);
 
@@ -39,17 +41,16 @@ public class LogoutService {
         jwtTokenCookie.setHttpOnly(true);
         jwtTokenCookie.setPath("/");
         jwtTokenCookie.setDomain("localhost");
-
         Cookie jwtRefreshTokenCookie = new Cookie("refreshToken", null);
         jwtRefreshTokenCookie.setMaxAge(0);
         jwtRefreshTokenCookie.setSecure(false);
         jwtRefreshTokenCookie.setHttpOnly(true);
         jwtRefreshTokenCookie.setPath("/");
         jwtRefreshTokenCookie.setDomain("localhost");
-
         response.addCookie(jwtTokenCookie);
         response.addCookie(jwtRefreshTokenCookie);
-        return true;
+        
+        return resFactory.success(UserResMsg.USER_LOGOUT_SUCCESS, null);
     }
 
     public Optional<String> readServletCookie(HttpServletRequest request, String name){
