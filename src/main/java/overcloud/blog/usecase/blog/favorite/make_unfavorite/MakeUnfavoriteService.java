efde9296d166519c155f558f5462be3fa0d36fcd
shinymilotic <trungtin.mai1412@gmail.com>
2024-06-27 14:46:44 +0700

    update code

---
+++ src/main/java/overcloud/blog/usecase/blog/favorite/make_unfavorite/MakeUnfavoriteService.java
@@ -0,0 +1,48 @@
+package overcloud.blog.usecase.blog.favorite.make_unfavorite;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import overcloud.blog.entity.ArticleEntity;
+import overcloud.blog.entity.FavoriteId;
+import overcloud.blog.entity.UserEntity;
+import overcloud.blog.repository.IArticleRepository;
+import overcloud.blog.repository.IFavoriteRepository;
+import overcloud.blog.usecase.blog.common.ArticleResMsg;
+import overcloud.blog.usecase.common.auth.service.SpringAuthenticationService;
+import overcloud.blog.usecase.common.exceptionhandling.InvalidDataException;
+import overcloud.blog.usecase.common.response.ResFactory;
+import overcloud.blog.usecase.common.response.RestResponse;
+import overcloud.blog.usecase.user.common.UserResMsg;
+
+@Service
+public class MakeUnfavoriteService {
+    private final IFavoriteRepository favoriteRepository;
+    private final SpringAuthenticationService authenticationService;
+    private final IArticleRepository articleRepository;
+    private final ResFactory resFactory;
+
+    public MakeUnfavoriteService(IFavoriteRepository favoriteRepository,
+                                 SpringAuthenticationService authenticationService,
+                                 IArticleRepository articleRepository,
+                                 ResFactory resFactory) {
+        this.favoriteRepository = favoriteRepository;
+        this.authenticationService = authenticationService;
+        this.articleRepository = articleRepository;
+        this.resFactory = resFactory;
+    }
+
+    @Transactional
+    public RestResponse<Void> makeUnfavorite(String slug) {
+        ArticleEntity articleEntity = articleRepository.findBySlug(slug).get(0);
+        UserEntity currentUser = authenticationService.getCurrentUser()
+                .orElseThrow(() -> new InvalidDataException(resFactory.fail(UserResMsg.USER_NOT_FOUND)))
+                .getUser();
+
+        FavoriteId favoritePk = new FavoriteId();
+        favoritePk.setUserId(currentUser.getId());
+        favoritePk.setArticleId(articleEntity.getId());
+        favoriteRepository.deleteById(favoritePk);
+        
+        return resFactory.success(ArticleResMsg.ARTICLE_UNFAVORITE_SUCCESS, null);
+    }
+}
