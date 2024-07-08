3bfc753708c99c52c5a157ccf68cae63fb81ca65
shinymilotic <trungtin.mai1412@gmail.com>
2024-07-08 22:44:40 +0700

    update code

--- src/main/java/overcloud/blog/usecase/user/update_user/UpdateUserRequest.java
+++ src/main/java/overcloud/blog/usecase/user/update_user/UpdateUserRequest.java
@@ -19,10 +19,6 @@ public class UpdateUserRequest {
     @LetterAndNumber(message = "user.register.username.letter-and-number")
     private String username;
 
-    @JsonProperty("email")
-    @NotBlank(message = "user.register.email.not-blank")
-    @Email(message = "user.register.email.valid")
-    private String email;
     @JsonProperty("bio")
     @Size(min = 8, max = 1000, message = "user.profile.bio.size")
     private String bio;
