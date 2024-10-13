b7d9be008800c3f89bd832afb5456dffd29e60f7
shinymilotic <trungtin.mai1412@gmail.com>
2024-10-13 16:12:55 +0700

    delete newline

--- src/main/java/overcloud/blog/interceptors/AuthInterceptor.java
+++ src/main/java/overcloud/blog/interceptors/AuthInterceptor.java
@@ -6,7 +6,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
-
 import overcloud.blog.exception.InvalidDataException;
 import overcloud.blog.response.ResFactory;
 import overcloud.blog.entity.RoleEntity;
