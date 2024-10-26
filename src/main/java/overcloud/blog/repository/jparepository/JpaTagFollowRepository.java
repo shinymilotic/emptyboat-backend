e7da25cf456126132ed0d81527515bd2b7890416
shinymilotic <trungtin.mai1412@gmail.com>
2024-10-26 10:13:02 +0700

    changes

--- src/main/java/overcloud/blog/repository/jparepository/JpaTagFollowRepository.java
+++ src/main/java/overcloud/blog/repository/jparepository/JpaTagFollowRepository.java
@@ -3,8 +3,9 @@ package overcloud.blog.repository.jparepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import overcloud.blog.entity.TagFollowEntity;
+import overcloud.blog.entity.TagFollowId;
 
 @Repository
-public interface JpaTagFollowRepository extends JpaRepository<TagFollowEntity, TagFollowEntity> {
+public interface JpaTagFollowRepository extends JpaRepository<TagFollowEntity, TagFollowId> {
 
 }
