8f3b52ff2ffb2f8724da164b2e29067b7ae85bf2
shinymilotic <trungtin.mai1412@gmail.com>
2024-11-09 20:35:50 +0700

    change

--- src/main/java/overcloud/blog/repository/jparepository/JpaChoiceAnswerRepository.java
+++ src/main/java/overcloud/blog/repository/jparepository/JpaChoiceAnswerRepository.java
@@ -1,6 +1,5 @@
 package overcloud.blog.repository.jparepository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
