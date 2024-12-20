46bd8d60581111dffd63fab1e1c7803ac6c677ce
shinymilotic <trungtin.mai1412@gmail.com>
2024-12-20 11:05:38 +0700

    update message

--- src/main/java/overcloud/blog/usecase/test/common/PracticeResMsg.java
+++ src/main/java/overcloud/blog/usecase/test/common/PracticeResMsg.java
@@ -5,7 +5,6 @@ public class PracticeResMsg  {
     public static final String PRACTICE_NOT_FOUND = "practice.not-found";
     public static final String PRACTICE_CREATE_SUCCESS = "practice.create.success";
     public static final String PRACTICE_CREATE_FAILED = "practice.create.failed";
-    public static final String PRACTICE_UPDATE_SUCCESS = "practice.update.success";
     public static final String PRACTICE_GETRESULT_SUCCESS = "practice.get-result";
     public static final String PRACTICE_GET_LIST = "practice.get-list";
 }
