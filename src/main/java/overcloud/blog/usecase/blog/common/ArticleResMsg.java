b94865c596bdab1652a80c1d38850c09c134fe2f
shinymilotic <trungtin.mai1412@gmail.com>
2025-02-06 21:03:51 +0700

    update

--- src/main/java/overcloud/blog/usecase/blog/common/ArticleResMsg.java
+++ src/main/java/overcloud/blog/usecase/blog/common/ArticleResMsg.java
@@ -2,10 +2,6 @@ package overcloud.blog.usecase.blog.common;
 
 
 public class ArticleResMsg {
-    public static final String ARTICLE_CREATE_SUCCESS = "article.create.success";
-    public static final String ARTICLE_CREATE_FAILED = "article.create.failed";
-    public static final String ARTICLE_UPDATE_SUCCESS = "article.update.success";
-    public static final String ARTICLE_UPDATE_FAILED = "article.update.failed";
     public static final String ARTICLE_UPDATE_NO_AUTHORIZATION = "article.update.no-authorization";
     public static final String ARTICLE_NO_EXISTS = "article.no-exists";
     public static final String ARTICLE_TITLE_EXISTS = "article.title-exist";
@@ -14,9 +10,4 @@ public class ArticleResMsg {
     public static final String ARTICLE_DESCRIPTION_SIZE = "article.description.size";
     public static final String ARTICLE_BODY_NOTBLANK = "article.body.not-blank";
     public static final String ARTICLE_TAGS_NOTEMPTY = "article.tags.not-empty";
-    public static final String ARTICLE_DELETE_SUCCESS = "article.delete.success";
-    public static final String ARTICLE_FAVORITE_SUCCESS = "article.favorite.success";
-    public static final String ARTICLE_UNFAVORITE_SUCCESS = "article.unfavorite.success";
-    public static final String ARTICLE_GET_SUCCESS = "article.get.success";
-    public static final String ARTICLE_GET_LIST = "article.get-list.success";
 }
