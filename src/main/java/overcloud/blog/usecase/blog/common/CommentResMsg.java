5ee6c7ccc5002e371d6e2099898acc75d0247be7
shinymilotic <trungtin.mai1412@gmail.com>
2025-02-06 21:41:12 +0700

    update

--- src/main/java/overcloud/blog/usecase/blog/common/CommentResMsg.java
+++ src/main/java/overcloud/blog/usecase/blog/common/CommentResMsg.java
@@ -2,10 +2,6 @@ package overcloud.blog.usecase.blog.common;
 
 
 public class CommentResMsg  {
-    public static final String COMMENT_SUCCESS = "comment.success";
     public static final String COMMENT_ARTICLE_NOT_EXIST = "comment.article.not-exist";
     public static final String COMMENT_NOTBLANK = "comment.not-blank";
-    public static final String COMMENT_DELETE_SUCCESS = "comment.delete.success";
-    public static final String COMMENT_CREATE_FAILED = "comment.create.failed";
-    public static final String COMMENT_GET_SUCCESS = "comment.get.success";
 }
