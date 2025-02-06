79e9226a6779981e33d10c20774826a866149ec8
shinymilotic <trungtin.mai1412@gmail.com>
2025-02-06 21:43:37 +0700

    update

--- src/main/java/overcloud/blog/usecase/test/common/PracticeResMsg.java
+++ src/main/java/overcloud/blog/usecase/test/common/PracticeResMsg.java
@@ -2,9 +2,5 @@ package overcloud.blog.usecase.test.common;
 
 
 public class PracticeResMsg  {
-    public static final String PRACTICE_NOT_FOUND = "practice.not-found";
-    public static final String PRACTICE_CREATE_SUCCESS = "practice.create.success";
     public static final String PRACTICE_CREATE_FAILED = "practice.create.failed";
-    public static final String PRACTICE_GETRESULT_SUCCESS = "practice.get-result";
-    public static final String PRACTICE_GET_LIST = "practice.get-list";
 }
--- src/main/java/overcloud/blog/usecase/test/common/TestResMsg.java
+++ src/main/java/overcloud/blog/usecase/test/common/TestResMsg.java
@@ -2,17 +2,11 @@ package overcloud.blog.usecase.test.common;
 
 
 public class TestResMsg  {
-    public static final String TEST_CREATE_SUCCESS = "test.create.success";
     public static final String TEST_NOT_FOUND = "test.not-found";
-    public static final String TEST_GET_LIST = "test.get-list";
-    public static final String TEST_GET_SUCCESS = "test.get.success";
-    public static final String TEST_PROFILE_GET_SUCCESS = "test.profile.get.success";
     public static final String TEST_TITLE_SIZE = "test.title.size";
     public static final String TEST_LIST_QUESTION_SIZE = "test.questions.size";
     public static final String TEST_QUESTION_SIZE = "test.question.not-blank";
     public static final String TEST_LIST_ANSWER_SIZE = "test.answers.size";
     public static final String TEST_ANSWER_SIZE = "test.answer.not-blank";
     public static final String TEST_AUTHOR_NOT_MATCH = "test.author.not-match";
-    public static final String TEST_UPDATE_SUCCESS = "test.update.success";
-    public static final String TEST_DELETE_SUCCESS = "test.delete.success";
 }
