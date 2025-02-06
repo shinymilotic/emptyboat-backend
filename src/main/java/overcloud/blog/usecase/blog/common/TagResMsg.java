0bfbe339d582925c0660202e3539927958c58fac
shinymilotic <trungtin.mai1412@gmail.com>
2025-02-06 21:43:56 +0700

    update

--- src/main/java/overcloud/blog/usecase/blog/common/TagResMsg.java
+++ src/main/java/overcloud/blog/usecase/blog/common/TagResMsg.java
@@ -3,13 +3,4 @@ package overcloud.blog.usecase.blog.common;
 public class TagResMsg  {
     public static final String TAG_EXISTS = "tag.exists";
     public static final String TAG_NO_EXISTS = "tag.no-exists";
-    // create tags
-    public static final String TAG_CREATE_SUCCESS = "tag.create.success";
-    // create tags failed
-    public static final String TAG_CREATE_FAILED = "tag.create.failed";
-    // get tags success
-    public static final String TAG_GET_SUCCESS = "tag.get.success";
-    public static final String FOLLOWING_TAGS = "tag.following-tags.success";
-    public static final String TAG_FOLLOW_SUCCESS = "tag.follow.success";
-    public static final String TAGS_UNFOLLOW_SUCCESS = "tag.unfollow.success";
 }
