00e3ff129d8760ec304642e10b48fe495d6a4c60
shinymilotic <trungtin.mai1412@gmail.com>
2025-03-07 18:50:47 +0700

    update spring

--- src/main/java/overcloud/blog/repository/jparepository/JpaUserRepository.java
+++ src/main/java/overcloud/blog/repository/jparepository/JpaUserRepository.java
@@ -5,8 +5,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import overcloud.blog.entity.UserEntity;
-
-import java.util.List;
 import java.util.UUID;
 
 @Repository
